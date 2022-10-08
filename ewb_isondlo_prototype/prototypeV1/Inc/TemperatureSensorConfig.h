6c98f4b0ed1e0e1c5a1116f079f07eef96c8b409
Bonga Njamela <75367761+BongaNjamela001@users.noreply.github.com>
2022-10-08 12:52:30 +0200

    Update TemperatureSensorConfig.h
    DS18B20 setup header file. Added setup(), lower_trigger and upper_trigger functions.

--- ewb_isondlo_prototype/prototypeV1/Inc/TemperatureSensorConfig.h
+++ ewb_isondlo_prototype/prototypeV1/Inc/TemperatureSensorConfig.h
@@ -2,7 +2,8 @@
 /**
   *********************************************************************************************************
   * TemperatureSensorConfig.h - Library for configuring the DS18B20 temperature sensing module to connect
-  * with UNO R3. 
+  * with UNO R3. The module works for temperature between -25C to +125C. Outputs 9 to 12 bit 
+  * centigrade value. Enable user-programmable triggers for high and low temperatures.
   * Created by the Engineers Without Borders Electronics Team for the iSondlo Garden Project
   * October 2022
   ********************************************************************************************************* 
@@ -11,9 +12,13 @@
 #ifndef TemperatureSensorConfig_h
 #define TemperatureSensorConfig_h
 
-#include "Arduino.h"
-
 class TemperatureSensorConfig {
-    
-}
+    public:
+       void setup();
+       void lower_trigger(int lower_temperature);
+       void upper_trigger(int high_temperature);
+       void clear();
+};
+
+extern TemperatureSensorConfig temperaturesensorconfig;
 #endif
