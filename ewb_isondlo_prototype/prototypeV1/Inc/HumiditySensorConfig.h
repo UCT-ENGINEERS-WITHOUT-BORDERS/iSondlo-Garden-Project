1242c8fe95d0bdfe0446ef1951bfedcc1f86e839
Bonga Njamela <75367761+BongaNjamela001@users.noreply.github.com>
2022-10-08 12:28:16 +0200

    Update HumiditySensorConfig.h
    setup() and baseline() functions added. Baseline sets a baseline relative humidity so that an alarm can be triggered for values below the baseline humidity

--- ewb_isondlo_prototype/prototypeV1/Inc/HumiditySensorConfig.h
+++ ewb_isondlo_prototype/prototypeV1/Inc/HumiditySensorConfig.h
@@ -1,7 +1,9 @@
 /* Humidity Sensor Module Configuration Library*/
 /**
   *********************************************************************************************************
-  * HumiditySensorConfig.h - Library for configuring the humidity sensor. 
+  * HumiditySensorConfig.h - Library for configuring the Ghitron HMZ-333A humidity sensor. This module
+  * consists of an HCZ sensor and an integrated circuit which gives a linear DC Voltage output for
+  * 0-100% RH.
   * Created by the Engineers Without Borders Electronics Team for the iSondlo Garden Project
   * October 2022
   ********************************************************************************************************* 
@@ -10,10 +12,11 @@
 #ifndef HumiditySensorConfig_h
 #define HumiditySensorConfig_h
 
-#include "Arduino.h"
-
 class HumiditySensorConfig {
-    
-}
+    public:
+       void setup();
+       void baseline(int baseline_humidity);
+};
 
+extern HumiditySensorConfig humiditysensorconfig;
 #endif
