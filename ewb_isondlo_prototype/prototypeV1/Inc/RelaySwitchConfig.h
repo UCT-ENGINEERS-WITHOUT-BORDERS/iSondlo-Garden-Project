fbee3432db16d42d32121c9da45bcdae2311ecd6
Bonga Njamela <75367761+BongaNjamela001@users.noreply.github.com>
2022-10-08 12:59:55 +0200

    Update RelaySwitchConfig.h

--- ewb_isondlo_prototype/prototypeV1/Inc/RelaySwitchConfig.h
+++ ewb_isondlo_prototype/prototypeV1/Inc/RelaySwitchConfig.h
@@ -1,1 +1,20 @@
- 
+/* Relay Switch Configuration Library*/
+/**
+  *********************************************************************************************************
+  * RelaySwitchConfig library for setting up the single relay module. Module is active HIGH. Intended for 
+  * use with solenoid valve. 
+  * Created by the Engineers Without Borders Electronics Team for the iSondlo Garden Project
+  * October 2022
+  ********************************************************************************************************* 
+  */
+
+#ifndef RelaySwitchConfig_h
+#define RelaySwitchConfig_h
+
+class RelaySwitchConfig {
+    public:
+       void setup();
+};
+
+extern RelaySwitchConfig relayswitchconfig;
+#endif
