f2bd462a34ae9cb3de025681c8f538b2fd4a97ed
Bonga Njamela <75367761+BongaNjamela001@users.noreply.github.com>
2022-10-08 12:44:59 +0200

    Update SoilMoistureSensorConfig.h

--- ewb_isondlo_prototype/prototypeV1/Inc/SoilMoistureSensorConfig.h
+++ ewb_isondlo_prototype/prototypeV1/Inc/SoilMoistureSensorConfig.h
@@ -3,6 +3,9 @@
   *********************************************************************************************************
   * SoilMoistureSensorConfig.h - Library for configuring the connection between the
   * arduino and the KE2023 KEYES soil moisture sensor module. 
+  * 0~300 dry soil
+  * 300~700 humid soil
+  * 700~950 in water
   * Created by the Engineers Without Borders Electronics Team for the iSondlo Garden Project
   * October 2022
   ********************************************************************************************************* 
@@ -11,10 +14,12 @@
 #ifndef SoilMoistureSensorConfig_h
 #define SoilMoistureSensorConfig_h
 
-#include "Arduino.h"
-
 class SoilMoistureSensorConfig {
-    
-}
+    public:
+       void setup();
+       void baseline(int soilmoisture_baseline);
+};
+
+extern SoilMoistureSensorConfig soilmoisturesensorconfig;
 #endif
 
